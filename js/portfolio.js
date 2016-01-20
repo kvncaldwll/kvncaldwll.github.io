e64c44cfeaec0bb4e07b774776e7bac2a12c96c6
keivn <kevin.caldwell@students.makeschool.com>
2016-01-20 13:49:14 -0800

    gallery page

---
+++ js/portfolio.js
@@ -0,0 +1,13 @@
+$( document ).ready(function() {
+	var gallery = $(".gallery").justifiedGallery({
+		rowHeight : 200,
+		lastRow : 'justify',
+		margins : 3,
+		randomize: true
+	});
+
+	gallery.on('jg.complete', function() {
+        $('.gallery a').swipebox({hideBarsDelay : false});
+        $('.gallery a').swipebox({loopAtEnd: true});
+    });
+});
