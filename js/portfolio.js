b46b8c9bb271088e1ff83ab44c5e706f8e20e295
keivn c <keivnc@keivns-MacBook-Pro.local>
2016-05-30 13:02:03 -0700

    gallery images

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,3 +1,34 @@
 $( document ).ready(function() {
-    
+    var gallery = $("#viewtiful").justifiedGallery({
+        rowHeight : 100,
+        maxRowHeight : 400,
+        lastRow : 'justify',
+        margins : 3,
+    });
+
+    gallery.on('jg.complete', function() {
+        $('#viewtiful a').swipebox({hideBarsDelay : false})
+    });
+
+    var gallery = $("#xBAND").justifiedGallery({
+        rowHeight : 100,
+        maxRowHeight : 400,
+        lastRow : 'justify',
+        margins : 3,
+    });
+
+    gallery.on('jg.complete', function() {
+        $('#xBAND a').swipebox({hideBarsDelay : false})
+    });
+
+    var gallery = $("#ss").justifiedGallery({
+        rowHeight : 100,
+        maxRowHeight : 400,
+        lastRow : 'justify',
+        margins : 3,
+    });
+
+    gallery.on('jg.complete', function() {
+        $('#ss a').swipebox({hideBarsDelay : false})
+    });
 });
